bc672c1ddf65a5430795163ebb8f84477f124ee5
Andrey Kurilov <andrey.kurilov@emc.com>
2014-11-06 10:30:15 +0300

    Fixed index.jspx

--- src/main/java/com/emc/mongoose/web/ui/MainServlet.java
+++ src/main/java/com/emc/mongoose/web/ui/MainServlet.java
@@ -1,34 +1,36 @@
 package com.emc.mongoose.web.ui;
-
+//
 import com.emc.mongoose.util.conf.RunTimeConfig;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
+//
+//import org.apache.logging.log4j.LogManager;
+//import org.apache.logging.log4j.Logger;
+//
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+//
 import java.io.IOException;
-
 /**
  * Created by gusakk on 02/10/14.
  */
-public class MainServlet extends HttpServlet {
-
-	private static final Logger LOG = LogManager.getLogger();
+public final class MainServlet
+extends HttpServlet {
+	//
+	//private static final Logger LOG = LogManager.getLogger();
 	private RunTimeConfig runTimeConfig;
-
+	//
 	@Override
-	public void init() throws ServletException {
+	public final void init()
+	throws ServletException {
 		runTimeConfig = (RunTimeConfig) getServletContext().getAttribute("runTimeConfig");
 		super.init();
 	}
-
-	public void doGet(HttpServletRequest request, HttpServletResponse response)
+	//
+	public final void doGet(final HttpServletRequest request, final HttpServletResponse response)
 	throws ServletException, IOException {
 		request.setAttribute("runmodes", RunModes.values());
 		request.setAttribute("runTimeConfig", runTimeConfig);
 		request.getRequestDispatcher("index.jspx").forward(request, response);
 	}
-
 }
