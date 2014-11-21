75f4e39dd76098321a664678e0702ccaf45e9b4e
Andrey Kurilov <andrey.kurilov@emc.com>
2014-11-21 14:24:31 +0300

    merged v0.4 fixes into v0.4.1

--- src/main/java/com/emc/mongoose/base/load/LoadExecutor.java
+++ src/main/java/com/emc/mongoose/base/load/LoadExecutor.java
@@ -28,10 +28,10 @@ extends Producer<T>, Consumer<T> {
 	//
 	static String
 		MSG_FMT_METRICS = "count=(%d/%d/%d); " +
-			"duration[s]=(%.6f/%.6f/%.6f/%.6f); latency[s]=(%.6f/%.6f/%.6f/%.6f); " +
+			"latency[s]=(%.6f/%.6f/%.6f/%.6f); duration[s]=(%.6f/%.6f/%.6f/%.6f); " +
 			"TP[/s]=(%.3f/%.3f/%.3f/%.3f); BW[MB/s]=(%.3f/%.3f/%.3f/%.3f)",
 		MSG_FMT_SUM_METRICS = "%s: count=(%d/%d); " +
-			"duration[s]=(%.6f/%.6f/%.6f/%.6f); latency[s]=(%.6f/%.6f/%.6f/%.6f); " +
+			"latency[s]=(%.6f/%.6f/%.6f/%.6f); duration[s]=(%.6f/%.6f/%.6f/%.6f); " +
 			"TP[/s]=(%.3f/%.3f/%.3f/%.3f); BW[MB/s]=(%.3f/%.3f/%.3f/%.3f)";
 	//
 	String getName()
