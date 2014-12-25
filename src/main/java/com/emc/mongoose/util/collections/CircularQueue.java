f83521fa35ae267aba90d86cb38bf0f50c206270
gusakk <Kirill.Gusakov@emc.com>
2014-12-26 00:48:34 +0400

    added finals

--- src/main/java/com/emc/mongoose/util/collections/CircularQueue.java
+++ src/main/java/com/emc/mongoose/util/collections/CircularQueue.java
@@ -5,18 +5,18 @@ import java.util.concurrent.ConcurrentLinkedQueue;
 /**
  * Created by gusakk on 12/12/14.
  */
-public class CircularQueue<E>
+public final class CircularQueue<E>
 extends ConcurrentLinkedQueue<E> {
 	//
 	private final int capacity;
 	//
-	public CircularQueue(int capacity) {
+	public CircularQueue(final int capacity) {
 		super();
 		this.capacity = capacity;
 	}
 	//
 	@Override
-	public boolean add(E e) {
+	public final boolean add(final E e) {
 		if (size() >= capacity) {
 			super.poll();
 		}
