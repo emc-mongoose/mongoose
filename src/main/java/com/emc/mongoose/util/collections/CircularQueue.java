b3e4d43aea583bc1e0b357eb1f13693c5e7a8941
gusakk <Kirill.Gusakov@emc.com>
2014-12-26 00:46:53 +0400

    renamed

--- src/main/java/com/emc/mongoose/util/collections/CircularQueue.java
+++ src/main/java/com/emc/mongoose/util/collections/CircularQueue.java
@@ -5,12 +5,12 @@ import java.util.concurrent.ConcurrentLinkedQueue;
 /**
  * Created by gusakk on 12/12/14.
  */
-public class CircularConcurrentLinkedQueue<E>
+public class CircularQueue<E>
 extends ConcurrentLinkedQueue<E> {
 	//
 	private final int capacity;
 	//
-	public CircularConcurrentLinkedQueue(int capacity) {
+	public CircularQueue(int capacity) {
 		super();
 		this.capacity = capacity;
 	}
