9812fc4b478b47696bf3b970b430ee56f48534b1
gusakk <Kirill.Gusakov@emc.com>
2015-04-20 16:16:57 +0300

    fix #263

--- src/main/webapp/js/script.js
+++ src/main/webapp/js/script.js
@@ -307,6 +307,17 @@ $(document).ready(function() {
 		parent.show();
 	});*/
 	//
+	$("#backup-run\\.id, #run\\.id").change(function() {
+		var startBtn = $("#start");
+		var currVal = this.value;
+		var patternValidId = /^[a-zA-Z]([-a-zA-Z0-9]*)$/;
+		if (!patternValidId.test(currVal)) {
+			alert("Invalid id value. RegExp for id: /^[a-zA-Z]([-a-zA-Z0-9]*)$/");
+			startBtn.prop("disabled", true);
+		} else {
+			startBtn.prop("disabled", false);
+		}
+	});
 });
 
 /* Functions */
