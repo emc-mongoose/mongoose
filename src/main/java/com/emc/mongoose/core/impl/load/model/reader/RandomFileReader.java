f6298a4e05ff95c8e4208403a9bba747bc79f2c9
zhavzo <Olga.Zhavzharova@emc.com>
2015-05-20 16:28:38 +0400

    comment for LOG in RandomFileReader

--- src/main/java/com/emc/mongoose/core/impl/load/model/reader/RandomFileReader.java
+++ src/main/java/com/emc/mongoose/core/impl/load/model/reader/RandomFileReader.java
@@ -1,9 +1,9 @@
 package com.emc.mongoose.core.impl.load.model.reader;
 //mongoose-common.jar
-//import com.emc.mongoose.common.logging.LogUtil;
+import com.emc.mongoose.common.logging.LogUtil;
 //
-//import org.apache.logging.log4j.LogManager;
-//import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 //
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -16,7 +16,7 @@ import java.util.Vector;
 public final class RandomFileReader
 implements AutoCloseable{
 
-	//private final static Logger LOG = LogManager.getLogger();
+	private final static Logger LOG = LogManager.getLogger();
 
 	private final BufferedReader reader;
 	private final Vector<String> linesBuffer;
