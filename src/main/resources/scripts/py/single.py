884a4c82f5def3b397b2f39ce64642ad47a968f5
zhavzo <Olga.Zhavzharova@emc.com>
2015-06-10 11:59:11 +0400

    fix logger in scripts

--- src/main/resources/scripts/py/single.py
+++ src/main/resources/scripts/py/single.py
@@ -2,7 +2,7 @@ from timeout import init as timeOutInit
 from loadbuilder import init as loadBuilderInit
 #
 from com.emc.mongoose.common.conf import RunTimeConfig
-from com.emc.mongoose.common.logging import LogUtil
+from com.emc.mongoose.common.logging import LogUtil, Markers
 #
 from com.emc.mongoose.core.api.io.task import IOTask
 #
@@ -19,18 +19,18 @@ def init():
 		loadType = IOTask.Type.valueOf(
 			RunTimeConfig.getContext().getString(RunTimeConfig.KEY_SCENARIO_SINGLE_LOAD).upper()
 		)
-		LOG.debug(LogUtil.MSG, "Using load type: {}", loadType.name())
+		LOG.debug(Markers.MSG, "Using load type: {}", loadType.name())
 		loadBuilder.setLoadType(loadType)
 	except NoSuchElementException:
-		LOG.error(LogUtil.ERR, "No load type specified, try arg -Dscenario.single.load=<VALUE> to override")
+		LOG.error(Markers.ERR, "No load type specified, try arg -Dscenario.single.load=<VALUE> to override")
 	except IllegalArgumentException:
-		LOG.error(LogUtil.ERR, "No such load type, it should be a constant from Load.Type enumeration")
+		LOG.error(Markers.ERR, "No such load type, it should be a constant from Load.Type enumeration")
 	return loadBuilder
 #
 def build(loadBuilder):
 	load = None
 	if loadBuilder is None:
-		LOG.fatal(LogUtil.ERR, "No load builder specified")
+		LOG.fatal(Markers.ERR, "No load builder specified")
 	else:
 		try:
 			load = loadBuilder.build()
@@ -40,7 +40,7 @@ def build(loadBuilder):
 #
 def execute(load):
 	if load is None:
-		LOG.fatal(LogUtil.ERR, "No load job specified")
+		LOG.fatal(Markers.ERR, "No load job specified")
 	else:
 		runTimeOut = timeOutInit()
 		load.start()
@@ -55,9 +55,9 @@ if __name__ == "__builtin__":
 	try:
 		execute(load)
 	except InterruptedException as e:
-		LOG.debug(LogUtil.MSG, "Single was interrupted")
+		LOG.debug(Markers.MSG, "Single was interrupted")
 	except Throwable as e:
 		e.printStackTrace(System.err)
 		LogUtil.exception(LOG, Level.ERROR, e, "Scenario failed")
 	loadBuilder.close() # to exit normally
-	LOG.info(LogUtil.MSG, "Scenario end")
+	LOG.info(Markers.MSG, "Scenario end")
