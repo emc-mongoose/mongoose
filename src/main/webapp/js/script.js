ba59a682b087c17bdce230fb7f5b0b8c722d3a00
gusakk <Kirill.Gusakov@emc.com>
2015-06-17 20:55:55 +0300

    save

--- src/main/webapp/js/script.js
+++ src/main/webapp/js/script.js
@@ -468,6 +468,7 @@ function configureWebSocketConnection(location, countOfRecords) {
 		PERF_AVG: "perf-avg-csv"
 	};
 	function processJsonLogEvents(chartsArray, json) {
+		var threadNameRegExp = /([\d]+)-([A-Za-z0-9]+)-([CreateRdDlUpAn]+)[\d]*-([\d]*)x([\d]*)x?([\d]*)/gi;
 		var runId = json.contextMap["run.id"];
 		var runMetricsPeriodSec = json.contextMap["load.metricsPeriodSec"];
 		var scenarioChainLoad = json.contextMap["scenario.type.chain.load"];
@@ -532,9 +533,7 @@ function configureWebSocketConnection(location, countOfRecords) {
 							charts(chartsArray).single(json);
 							break;
 						case RUN_SCENARIO_NAME.chain:
-							if (json.threadName.indexOf("remote") > -1) {
-								json.threadName = json.threadName.substring(0, json.threadName.lastIndexOf("-"));
-							}
+							json.threadName = json.threadName.match(threadNameRegExp)[0];
 							charts(chartsArray).chain(runId, runMetricsPeriodSec, json.threadName);
 							break;
 					}
@@ -630,7 +629,7 @@ function charts(chartsArray) {
 		width = 1070 - margin.left - margin.right,
 		height = 460 - margin.top - margin.bottom;
 	//  Some constants
-	var threadNameRegExp = "/([\d]+)-([A-Za-z0-9]+)-([CreateRdDlUpAn]+)[\d]*-([\d]*)x([\d]*)x?([\d]*)/gi";
+	var threadNameRegExp = /([\d]+)-([A-Za-z0-9]+)-([CreateRdDlUpAn]+)[\d]*-([\d]*)x([\d]*)x?([\d]*)/gi;
 	//
 	var SCENARIO = {
 		single: "single",
@@ -759,7 +758,7 @@ function charts(chartsArray) {
 		//  get some fields from runTimeConfig
 		var runMetricsPeriodSec = json.contextMap[RUN_TIME_CONFIG_CONSTANTS.runMetricsPeriodSec];
         //
-		json.threadName = json.threadName.match(threadNameRegExp);
+		json.threadName = json.threadName.match(threadNameRegExp)[0];
         //
 		var currentMetricsPeriodSec = 0;
 		//var runScenarioName = json.contextMap[RUN_TIME_CONFIG_CONSTANTS.runScenarioName];
@@ -1477,7 +1476,7 @@ function charts(chartsArray) {
                         saveChart(path, 1070, 460);
                     });
 				return function(chartType, json) {
-					json.threadName = json.threadName.match(threadNameRegExp);
+					json.threadName = json.threadName.match(threadNameRegExp)[0];
 					var loadType = json.threadName;
 					//
 					var splitIndex = 0;
