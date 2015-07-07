3fecbf1193c274c976af4ec231c2a68768a418a6
gusakk <Kirill.Gusakov@emc.com>
2015-07-07 12:25:03 +0300

    fixed spaces

--- src/main/java/com/emc/mongoose/core/api/load/model/LoadState.java
+++ src/main/java/com/emc/mongoose/core/api/load/model/LoadState.java
@@ -23,31 +23,31 @@ public interface LoadState extends Serializable {
 	//
 	long[] getLatencyValues();
 	//
+	long getLoadElapsedTimeValue();
+	//
 	TimeUnit getLoadElapsedTimeUnit();
 	//
-	long getLoadElapsedTimeValue();
-    //
-    interface Builder<T> {
-        //
-        Builder<T> setLoadNumber(final int loadNumber);
-        //
-        Builder<T> setRunTimeConfig(final RunTimeConfig runTimeConfig);
-        //
-        Builder<T> setCountSucc(final long countSucc);
-        //
-        Builder<T> setCountFail(final long countFail);
-        //
-        Builder<T> setCountBytes(final long countBytes);
-        //
-        Builder<T> setCountSubm(final long countSubm);
-        //
-        Builder<T> setLatencyValues(final long[] latencyValues);
-        //
-        Builder<T> setLoadElapsedTimeValue(final long timeValue);
-        //
-        Builder<T> setLoadElapsedTimeUnit(final TimeUnit timeUnit);
-        //
-        T build();
-        //
-    }
+	interface Builder<T> {
+		//
+		Builder<T> setLoadNumber(final int loadNumber);
+		//
+		Builder<T> setRunTimeConfig(final RunTimeConfig runTimeConfig);
+		//
+		Builder<T> setCountSucc(final long countSucc);
+		//
+		Builder<T> setCountFail(final long countFail);
+		//
+		Builder<T> setCountBytes(final long countBytes);
+		//
+		Builder<T> setCountSubm(final long countSubm);
+		//
+		Builder<T> setLatencyValues(final long[] latencyValues);
+		//
+		Builder<T> setLoadElapsedTimeValue(final long timeValue);
+		//
+		Builder<T> setLoadElapsedTimeUnit(final TimeUnit timeUnit);
+		//
+		T build();
+		//
+	}
 }
