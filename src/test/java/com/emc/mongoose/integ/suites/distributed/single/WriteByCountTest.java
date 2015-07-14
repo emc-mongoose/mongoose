5181c913745ab5c1ee888d66b7020b629b1b80ae
Andrey Kurilov <andrey.kurilov@emc.com>
2015-07-14 18:01:40 +0300

    save

--- src/test/java/com/emc/mongoose/integ/suites/distributed/single/WriteByCountTest.java
+++ src/test/java/com/emc/mongoose/integ/suites/distributed/single/WriteByCountTest.java
@@ -21,7 +21,7 @@ import java.nio.file.Paths;
 public final class WriteByCountTest {
 	//
 	private final static OutputStream
-		STREAM_STD_INTERCEPTOR = new SavedOutputStream(System.out),
+		STREAM_STD_INTERCEPTOR = new SavedOutputStream(System.out);
 	private final static int DATA_COUNT = 100000;
 	private static Thread
 		WSMOCK_THREAD, LOAD_SERVER_THREAD;
@@ -40,41 +40,4 @@ public final class WriteByCountTest {
 		PERF_TRACE_FILE_NAME = "perf.trace.csv",
 		DATA_ITEMS_FILE_NAME = "data.items.csv";
 	//
-	@BeforeClass
-	public static void initLogging() {
-		LogUtil.init();
-	}
-	//
-	@AfterClass
-	public static void stopLogging() {
-		if (System.getProperty(LOG_CONF_PROPERTY_KEY) == null) {
-			String fullLogConfFile = Paths
-				.get(System.getProperty(USER_DIR_PROPERTY_NAME), Constants.DIR_CONF, LOG_FILE_NAME)
-				.toString();
-			System.setProperty(LOG_CONF_PROPERTY_KEY, fullLogConfFile);
-		}
-		LogUtil.shutdown();
-	}
-	//
-	@BeforeClass
-	public static void initRunTimeConfig() {
-		RunTimeConfig.initContext();
-	}
-	//
-	@BeforeClass
-	public static void startWSMock()
-	throws Exception {
-		WSMOCK_THREAD = new Thread(new Cinderella(RunTimeConfig.getContext()), "wsMock");
-		WSMOCK_THREAD.setDaemon(true);
-		WSMOCK_THREAD.start();
-	}
-	//
-	@AfterClass
-	public static void stopWSMock()
-	throws Exception {
-		WSMOCK_THREAD.interrupt();
-	}
-	//
-	@BeforeClass
-	public static void
 }
