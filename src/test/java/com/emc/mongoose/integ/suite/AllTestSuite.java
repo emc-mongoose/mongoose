7763452a879cc80b1a62ee354bfff74504095b80
Andrey Kurilov <andrey.kurilov@emc.com>
2015-09-10 12:18:53 +0300

    save

--- src/test/java/com/emc/mongoose/integ/suite/AllTestSuite.java
+++ src/test/java/com/emc/mongoose/integ/suite/AllTestSuite.java
@@ -7,7 +7,7 @@ import org.junit.runners.Suite;
 //
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-	//CoreTestSuite.class,
+	CoreTestSuite.class,
 	DistributedLoadTestSuite.class,
 	StorageAdapterTestSuite.class,
 })
