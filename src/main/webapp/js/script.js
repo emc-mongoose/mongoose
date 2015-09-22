ad2e9cf6a354c9b303d7f702b72553fba293f215
gusakk <Kirill.Gusakov@emc.com>
2015-09-22 19:04:19 +0300

    fixed jquery error

--- src/main/webapp/js/script.js
+++ src/main/webapp/js/script.js
@@ -1,3 +1,25 @@
+jQuery.fn.addChild = function(html) {
+	var target = $(this[0]);
+	var child = $(html);
+	child.appendTo(target);
+	return child;
+};
+//
+jQuery.fn.prependChild = function(html) {
+	var target = $(this[0]);
+	var child = $(html);
+	child.prependTo(target);
+	return child;
+};
+//
+(function($) {
+	$.strRemove = function(theTarget, theString) {
+		return $("<div/>").append(
+			$(theTarget, theString).remove().end()
+		).html();
+	};
+})(jQuery);
+
 require(["./requirejs/conf"], function() {
 	require(["d3js", "bootstrap", "./util/visvalingam"], function(d3, bootstrap, vis) {
 		$(document).ready(function() {
@@ -394,27 +416,6 @@ require(["./requirejs/conf"], function() {
 			}
 		}
 //
-		jQuery.fn.addChild = function(html) {
-			var target = $(this[0]);
-			var child = $(html);
-			child.appendTo(target);
-			return child;
-		};
-//
-		jQuery.fn.prependChild = function(html) {
-			var target = $(this[0]);
-			var child = $(html);
-			child.prependTo(target);
-			return child;
-		};
-//
-		(function($) {
-			$.strRemove = function(theTarget, theString) {
-				return $("<div/>").append(
-					$(theTarget, theString).remove().end()
-				).html();
-			};
-		})(jQuery);
 //
 		function generatePropertyPage() {
 			if (!$("#properties").is(":checked")) {
