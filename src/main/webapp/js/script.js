d82c2f9a4bebfd61a73d1470afc949dc53e7fcd5
gusakk <Kirill.Gusakov@emc.com>
2015-09-22 19:41:55 +0300

    deleted mainjs

--- src/main/webapp/js/script.js
+++ src/main/webapp/js/script.js
@@ -1,5 +1,28 @@
 require(["./requirejs/conf"], function() {
 	require(["d3js", "jquery", "./util/visvalingam"], function(d3, $, vis) {
+		//
+		$.fn.addChild = function(html) {
+			var target = $(this[0]);
+			var child = $(html);
+			child.appendTo(target);
+			return child;
+		};
+		//
+		$.fn.prependChild = function(html) {
+			var target = $(this[0]);
+			var child = $(html);
+			child.prependTo(target);
+			return child;
+		};
+		//
+		(function($) {
+			$.strRemove = function(theTarget, theString) {
+				return $("<div/>").append(
+					$(theTarget, theString).remove().end()
+				).html();
+			};
+		})(jQuery);
+		//
 		$(document).ready(function() {
 			$("#backup-run\\.mode").val($("#run\\.mode").val());
 			var WEBSOCKET_URL = "ws://" + window.location.host + "/logs";
@@ -393,28 +416,6 @@ require(["./requirejs/conf"], function() {
 				}
 			}
 		}
-		//
-		$.fn.addChild = function(html) {
-			var target = $(this[0]);
-			var child = $(html);
-			child.appendTo(target);
-			return child;
-		};
-		//
-		$.fn.prependChild = function(html) {
-			var target = $(this[0]);
-			var child = $(html);
-			child.prependTo(target);
-			return child;
-		};
-		//
-		(function($) {
-			$.strRemove = function(theTarget, theString) {
-				return $("<div/>").append(
-					$(theTarget, theString).remove().end()
-				).html();
-			};
-		})($);
 		//
 		function generatePropertyPage() {
 			if (!$("#properties").is(":checked")) {
