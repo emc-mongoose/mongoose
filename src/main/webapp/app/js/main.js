d48415008f4e80526962810dc68a2e35caa7ff16
gusakk <Kirill.Gusakov@emc.com>
2015-10-14 18:25:38 +0300

    save again

--- src/main/webapp/app/js/main.js
+++ src/main/webapp/app/js/main.js
@@ -1,8 +1,8 @@
 require(["./requirejs/conf"], function() {
-	require(["handlebars", "text!../templates/navbar.hbs", "text!../templates/conf.hbs",
+	require(["handlebars", "jquery", "text!../templates/navbar.hbs", "text!../templates/conf.hbs",
 				"text!./configuration/base.json",
 				"bootstrap", "util/pace/loading", "util/bootstrap/tabs"],
-		function(Handlebars, versionTemplate, confTemplate, config) {
+		function(Handlebars, $, versionTemplate, confTemplate, config) {
 
 			var context = {
 				version: "1.0.0"
@@ -14,7 +14,10 @@ require(["./requirejs/conf"], function() {
 			document.body.insertAdjacentHTML("afterbegin", html);
 
 			compiledTemplate = Handlebars.compile(confTemplate);
-			html = compiledTemplate(JSON.parse(config));
+
+			config = JSON.parse(config);
+
+			html = compiledTemplate(config);
 
 			document.getElementById("configuration").insertAdjacentHTML("beforeend", html);
 
@@ -26,6 +29,59 @@ require(["./requirejs/conf"], function() {
 					file.style.display = "none";
 				}
 			});
+
+			$.get("/main", function(json) {
+				traverse(json);
+			});
+
+			//  fill the configuration w/ default values
+			function traverse(jsonObject, fieldPrefix) {
+				for (var key in jsonObject) {
+					if (jsonObject.hasOwnProperty(key)) {
+						var propertyName = "";
+						if (fieldPrefix != null) {
+							if (fieldPrefix.length > 0) {
+								propertyName = fieldPrefix + "." + key;
+							} else {
+								propertyName = key;
+							}
+						}
+
+						if (!(typeof jsonObject[key] === "object")) {
+							var domElement = document.getElementById(propertyName);
+							if (domElement) {
+								if (domElement.classList.contains("complex")) {
+									fillComplexElement(domElement, jsonObject[key]);
+								}
+								domElement.value = jsonObject[key];
+							}
+						} else {
+							traverse(jsonObject[key], propertyName);
+						}
+					}
+				}
+			}
+
+			function fillComplexElement(domElement, value) {
+				var regExpPattern = /^([0-9]*)([a-zA-Z]+)$/;
+				if (regExpPattern.test(value)) {
+					var matcher = regExpPattern.exec(value);
+					domElement.querySelector("input").value = matcher[1];
+					var selectableElement = domElement.querySelector("select");
+					var options = selectableElement.options;
+					for (var option in options) {
+						if (options.hasOwnProperty(option)) {
+							option = options[option];
+							var searchPattern = new RegExp('^' + matcher[2]);
+							if (searchPattern.test(option.innerText)) {
+								selectableElement.value = option.innerText;
+								domElement.querySelector('input[type="hidden"]').value
+									= matcher[1] + matcher[2];
+							}
+						}
+					}
+				}
+			}
 		}
 	);
 });
