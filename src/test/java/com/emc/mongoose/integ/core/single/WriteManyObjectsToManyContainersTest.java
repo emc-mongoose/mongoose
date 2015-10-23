daa748e6f246fc91d2fa6a2f8ad4917dca228ccb
Andrey Kurilov <andrey.kurilov@emc.com>
2015-10-23 14:46:03 +0300

    test change

--- src/test/java/com/emc/mongoose/integ/core/single/WriteManyObjectsToManyContainersTest.java
+++ src/test/java/com/emc/mongoose/integ/core/single/WriteManyObjectsToManyContainersTest.java
@@ -1,6 +1,5 @@
 package com.emc.mongoose.integ.core.single;
 import com.emc.mongoose.common.conf.RunTimeConfig;
-import com.emc.mongoose.common.conf.SizeUtil;
 import com.emc.mongoose.common.log.Markers;
 import com.emc.mongoose.common.log.appenders.RunIdFileManager;
 import com.emc.mongoose.integ.base.WSMockTestBase;
@@ -21,6 +20,8 @@ import java.io.File;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.util.concurrent.TimeUnit;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 /**
  Created by andrey on 23.10.15.
  */
@@ -29,8 +30,8 @@ extends WSMockTestBase {
 	//
 	private static BufferingOutputStream STD_OUTPUT_STREAM;
 	private static final int
-		LIMIT_COUNT_OBJ = 1000,
-		LIMIT_COUNT_CONTAINER = 1000;
+		LIMIT_COUNT_OBJ = 100,
+		LIMIT_COUNT_CONTAINER = 100;
 	//
 	private static String RUN_ID_BASE = WriteManyObjectsToManyContainersTest.class.getCanonicalName();
 	private static int countContainerCreated = 0;
@@ -42,12 +43,12 @@ extends WSMockTestBase {
 		System.setProperty(RunTimeConfig.KEY_LOAD_ITEM_CLASS, "container");
 		System.setProperty(RunTimeConfig.KEY_STORAGE_MOCK_CONTAINER_CAPACITY, Integer.toString(LIMIT_COUNT_OBJ));
 		System.setProperty(RunTimeConfig.KEY_STORAGE_MOCK_CONTAINER_COUNT_LIMIT, Integer.toString(LIMIT_COUNT_CONTAINER));
-		System.setProperty(RunTimeConfig.KEY_DATA_SIZE, "4KB");
+		System.setProperty(RunTimeConfig.KEY_DATA_SIZE, "1KB");
 		WSMockTestBase.setUpClass();
 		final RunTimeConfig rtConfig = RunTimeConfig.getContext();
 		rtConfig.set(RunTimeConfig.KEY_LOAD_LIMIT_COUNT, Integer.toString(LIMIT_COUNT_CONTAINER));
 		rtConfig.set(RunTimeConfig.KEY_SCENARIO_SINGLE_LOAD, TestConstants.LOAD_CREATE);
-		rtConfig.set(RunTimeConfig.KEY_CREATE_CONNS, "100");
+		rtConfig.set(RunTimeConfig.KEY_CREATE_CONNS, "10");
 		RunTimeConfig.setContext(rtConfig);
 		//
 		final Logger logger = LogManager.getLogger();
@@ -68,36 +69,52 @@ extends WSMockTestBase {
 			final BufferedReader
 				in = Files.newBufferedReader(containerListFile.toPath(), StandardCharsets.UTF_8)
 		) {
-			do {
-				nextContainer = in.readLine();
-				if(nextContainer == null) {
-					break;
-				} else {
-					countContainerCreated ++;
-					rtConfig.set(RunTimeConfig.KEY_RUN_ID, RUN_ID_BASE + "_" + nextContainer);
-					rtConfig.set(RunTimeConfig.KEY_API_S3_BUCKET, nextContainer);
-					RunTimeConfig.setContext(rtConfig);
-					TimeUnit.SECONDS.sleep(1);
-					new ScriptMockRunner().run();
-				}
-			} while(true);
+			try(final BufferingOutputStream stdOutStream = StdOutInterceptorTestSuite
+					.getStdOutBufferingStream()
+			) {
+				do {
+					nextContainer = in.readLine();
+					if(nextContainer == null) {
+						break;
+					} else {
+						countContainerCreated++;
+						rtConfig.set(RunTimeConfig.KEY_RUN_ID, RUN_ID_BASE + "_" + nextContainer);
+						rtConfig.set(RunTimeConfig.KEY_API_S3_BUCKET, nextContainer);
+						RunTimeConfig.setContext(rtConfig);
+						new ScriptMockRunner().run();
+					}
+				} while(true);
+				TimeUnit.SECONDS.sleep(1);
+				STD_OUTPUT_STREAM = stdOutStream;
+			}
 		}
-		TimeUnit.SECONDS.sleep(1);
 		//
 		RunIdFileManager.flushAll();
 	}
 	//
 	@AfterClass
 	public  static void tearDownClass()
-		throws Exception {
+	throws Exception {
 		WSMockTestBase.tearDownClass();
 		System.setProperty(RunTimeConfig.KEY_STORAGE_MOCK_CONTAINER_CAPACITY, "1000000");
 		System.setProperty(RunTimeConfig.KEY_STORAGE_MOCK_CONTAINER_COUNT_LIMIT, "1000000");
 	}
 	//
 	@Test
-	public final void checkSomething()
+	public final void checkCreatedContainerCount()
 	throws Exception {
 		Assert.assertEquals(LIMIT_COUNT_CONTAINER, countContainerCreated);
+	}
+	//
+	@Test
+	public final void checkThatAllContainersAlreadyWereExisting() {
+		final String consoleOutput = STD_OUTPUT_STREAM.toString();
+		final Pattern p = Pattern.compile("Bucket \"[a-z0-9]+\" already exists");
+		final Matcher m = p.matcher(consoleOutput);
+		int countMatch = 0;
+		while(m.find()) {
+			countMatch ++;
+		}
+		LOG.info(Markers.MSG, countMatch);
 	}
 }
