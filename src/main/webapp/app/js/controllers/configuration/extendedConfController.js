80ae74efb5c0222f3d5f338e146bef56c78e99be
gusakk <Kirill.Gusakov@emc.com>
2015-11-24 12:11:21 +0300

    added extendedCfgCtrl

--- src/main/webapp/app/js/controllers/configuration/extendedConfController.js
+++ src/main/webapp/app/js/controllers/configuration/extendedConfController.js
@@ -1,5 +1,63 @@
 define(["handlebars"], function(Handlebars) {
-	function start() {
-		console.log("extended");
+	//
+	function start(props) {
+		render(props);
 	}
+	//
+	function render(props) {
+		var shortPropsMap = {};
+		var ul = $(".folders");
+		ul.show();
+		walkTreeMap(props, ul, shortPropsMap);
+		$("<li>").appendTo($("#run").parent().find("ul").first())
+			.addClass("file")
+			.append($("<a>", {
+				class: "props",
+				href: "#" + "run.id",
+				text: "id"
+			}));
+	}
+	//
+	function walkTreeMap(map, ul, shortsPropsMap, fullKeyName) {
+		$.each(map, function(key, value) {
+			var element;
+			var currKeyName = "";
+			//
+			if(!fullKeyName) {
+				currKeyName = key;
+			} else {
+				currKeyName = fullKeyName + "." + key;
+			}
+			//
+			if (!(value instanceof Object)) {
+				if(currKeyName === "run.mode")
+					return;
+				$("<li>").appendTo(ul)
+					.addClass("file")
+					.append($("<a>", {
+						class: "props",
+						href: "#" + currKeyName,
+						text: key
+					}));
+				shortsPropsMap[currKeyName] = value;
+			} else {
+				element = $("<ul>").appendTo(
+					$("<li>").prependTo(ul)
+						.append($("<label>", {
+							for: key,
+							text: key
+						}))
+						.append($("<input>", {
+							type: "checkbox",
+							id: key
+						}))
+					);
+				walkTreeMap(value, element, shortsPropsMap, currKeyName);
+			}
+		});
+	}
+	//
+	return {
+		start: start
+	};
 });
