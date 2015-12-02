caa29f58a9201e48ddf918bd5bcfbc4162e84ffc
gusakk <Kirill.Gusakov@emc.com>
2015-12-02 19:08:32 +0300

    almost done

--- src/main/webapp/js/requirejs/conf.js
+++ src/main/webapp/js/requirejs/conf.js
@@ -4,12 +4,13 @@
 requirejs.config({
 
 	"paths": {
-		"jquery": "../../libs/js/jquery",
-		"bootstrap": "../../libs/js/bootstrap",
-		"handlebars": "../../libs/js/handlebars",
-		"text": "../../libs/js/text",
-		"pace": "../../libs/js/pace",
-		"d3js": "../../libs/js/d3js"
+		"bootstrap": ["/webjars/bootstrap/3.3.2-1/js/bootstrap", "js/bootstrap"],
+		"d3js": ["/webjars/d3js/3.5.3/d3", "d3"],
+		"validate-js": ["/webjars/validate.js/0.8.0/validate"],
+		"jquery": ["/webjars/jquery/2.1.4/jquery", "jquery"],
+		"handlebars": ["/webjars/handlebars/4.0.2/handlebars", "handlebars"],
+		"pace": ["/webjars/pace/1.0.2/pace", "pace"],
+		"text": ["/webjars/requirejs-text/2.0.14-1/text", "text"]
 	},
 
 	"shim": {
