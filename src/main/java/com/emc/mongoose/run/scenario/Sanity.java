3df881f1cd6b95b3e5a704003e318be2e70bd04f
Andrey Kurilov <andrey.kurilov@emc.com>
2015-12-09 21:07:28 +0300

    modify the sanity scenario

--- src/main/java/com/emc/mongoose/run/scenario/Sanity.java
+++ src/main/java/com/emc/mongoose/run/scenario/Sanity.java
@@ -43,7 +43,7 @@ implements Runnable {
 	//
 	private final static short DEFAULT_NODE_COUNT = 2, DEFAULT_CONN_PER_NODE = 200;
 	private final static long DEFAULT_DATA_SIZE = SizeUtil.toSize("10MB");
-	private final static int DEFAULT_DATA_COUNT_MAX = 1000;
+	private final static int DEFAULT_DATA_COUNT_MAX = 10000;
 	public final static Logger LOG;
 	static {
 		LogUtil.init();
