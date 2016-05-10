2fcfd51d0cec1ce1717ae68085d0cb8b7940851c
Andrey Kurilov <andrey.kurilov@emc.com>
2016-05-10 19:04:17 +0300

    save

---
+++ src/test/java/com/emc/mongoose/common/conf/DefaultsSchemaTest.java
@@ -0,0 +1,18 @@
+package com.emc.mongoose.common.conf;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ Created by kurila on 10.05.16.
+ */
+@RunWith(Suite.class)
+public class DefaultsSchemaTest
+extends TestCase {
+	@Test
+	public void checkDefaultSchema() {
+
+	}
+}
