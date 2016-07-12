ba9273c812f5929ffba24636bd7b37d5a4697b8a
Ilya Kisliakovsky <ilya.kisliakovsky@emc.com>
2016-07-12 15:11:49 +0300

    lifecycle first steps

--- common/src/main/java/com/emc/mongoose/common/concurrent/LifeCycleBase.java
+++ common/src/main/java/com/emc/mongoose/common/concurrent/LifeCycleBase.java
@@ -1,10 +1,32 @@
 package com.emc.mongoose.common.concurrent;
 
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicReference;
+
 /**
  Created on 12.07.16.
  */
 public abstract class LifeCycleBase implements LifeCycle {
 
+	private AtomicReference<State> state;
 
+	public enum State {
+		STARTED, SHUTDOWN, INTERRUPTED
+	}
 
+	@Override
+	public void start()
+	throws IllegalStateException {
+
+	}
+
+	@Override
+	public void shutdown()
+	throws IllegalStateException {
+	}
+
+	@Override
+	public void interrupt()
+	throws IllegalStateException {
+	}
 }
