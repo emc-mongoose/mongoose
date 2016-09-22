d66c4735c3a94543bed1d206c17603381b6eb92b
Andrey Kurilov <andrey.kurilov@emc.com>
2016-09-22 20:32:08 +0300

    SuperMegaLambdaHttpRequestFactory

--- storage/driver-http-base/src/main/java/com/emc/mongoose/storage/driver/http/base/HttpDriver.java
+++ storage/driver-http-base/src/main/java/com/emc/mongoose/storage/driver/http/base/HttpDriver.java
@@ -3,8 +3,12 @@ package com.emc.mongoose.storage.driver.http.base;
 import com.emc.mongoose.model.api.io.task.IoTask;
 import com.emc.mongoose.model.api.item.Item;
 import com.emc.mongoose.model.api.load.Driver;
+import com.emc.mongoose.model.util.LoadType;
+import io.netty.handler.codec.http.HttpHeaders;
+import io.netty.handler.codec.http.HttpMethod;
 import io.netty.util.AttributeKey;
 
+import java.net.URISyntaxException;
 /**
  Created by kurila on 30.08.16.
  */
@@ -14,4 +18,11 @@ extends Driver<I, O> {
 	String SIGN_METHOD = "HmacSHA1";
 	
 	AttributeKey<IoTask> ATTR_KEY_IOTASK = AttributeKey.valueOf("ioTask");
+
+	HttpMethod getHttpMethod(final LoadType loadType);
+
+	String getDstUriPath(final I item, final O ioTask);
+
+	void applyCopyHeaders(final HttpHeaders httpHeaders, final I obj)
+	throws URISyntaxException;
 }
