c4c26f7489c1c1734bcf372adb716c9f09facd90
Ilya Kisliakovsky <ilya.kisliakovsky@emc.com>
2016-09-23 16:28:41 +0300

    DriverFactoryService

---
+++ monitor/src/main/java/com/emc/mongoose/monitor/driver/DriverFactoryService.java
@@ -0,0 +1,13 @@
+package com.emc.mongoose.monitor.driver;
+
+import com.emc.mongoose.common.net.Service;
+
+/**
+ Created on 23.09.16.
+ */
+public interface DriverFactoryService
+extends Service {
+
+
+
+}
