43e67fd9aad34eb8d331a11b5b94c541d690d372
Ilya Kisliakovsky <ilya.kisliakovsky@emc.com>
2016-09-28 15:52:45 +0300

    removed console logging from ServiceUtil

--- common/src/main/java/com/emc/mongoose/common/net/ServiceUtil.java
+++ common/src/main/java/com/emc/mongoose/common/net/ServiceUtil.java
@@ -1,5 +1,8 @@
 package com.emc.mongoose.common.net;
 
+import com.emc.mongoose.common.exception.OmgDoesNotPerformException;
+import com.emc.mongoose.common.exception.OmgLookAtMyConsoleException;
+
 import java.net.Inet4Address;
 import java.net.InetAddress;
 import java.net.MalformedURLException;
@@ -8,7 +11,6 @@ import java.net.SocketException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.rmi.Naming;
-import java.rmi.NoSuchObjectException;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
@@ -43,13 +45,11 @@ public abstract class ServiceUtil {
 			if(REGISTRY == null) {
 				try {
 					REGISTRY = LocateRegistry.createRegistry(REGISTRY_PORT);
-					System.out.println("RMI registry created");
 				} catch(final RemoteException e) {
 					try {
 						REGISTRY = LocateRegistry.getRegistry(REGISTRY_PORT);
-						System.out.println("Reusing already existing RMI registry");
 					} catch(final RemoteException ee) {
-						System.err.println("Failed to obtain a RMI registry");
+						throw new IllegalStateException(ee);
 					}
 				}
 			}
@@ -58,11 +58,12 @@ public abstract class ServiceUtil {
 		}
 	}
 
-	public static URI svcUri(final String svcName) {
+	public static URI svcUri(final String svcName)
+	throws SocketException, OmgLookAtMyConsoleException, OmgDoesNotPerformException {
 		try {
 			String hostName = System.getProperty(ServiceUtil.KEY_RMI_HOSTNAME);
 			if (null != hostName) {
-				hostName = getHostAddr();
+				hostName = NetUtil.getHostAddrString();
 			}
 			return new URI(
 				"rmi", null, hostName, REGISTRY_PORT, "/" + svcName, null, null
@@ -72,112 +73,43 @@ public abstract class ServiceUtil {
 		throw new IllegalArgumentException();
 	}
 
-	public static String getHostAddr() {
-		InetAddress addr = null;
-		//
-		try {
-			final Enumeration<NetworkInterface> netIfaces = NetworkInterface.getNetworkInterfaces();
-			NetworkInterface nextNetIface;
-			String nextNetIfaceName;
-			while(netIfaces.hasMoreElements()) {
-				nextNetIface = netIfaces.nextElement();
-				nextNetIfaceName = nextNetIface.getDisplayName();
-				if(!nextNetIface.isLoopback() && nextNetIface.isUp()) {
-					final Enumeration<InetAddress> addrs = nextNetIface.getInetAddresses();
-					while(addrs.hasMoreElements()) {
-						addr = addrs.nextElement();
-						if(Inet4Address.class.isInstance(addr)) {
-							System.out.println(
-								"Resolved external interface \"" + nextNetIfaceName +
-									"\" address: " + addr.getHostAddress()
-							);
-							break;
-						}
-					}
-				} else {
-					System.out.println(
-						"Interface \"" + nextNetIfaceName +
-							"\"is loopback or is not up, skipping"
-					);
-				}
-			}
-		} catch(final SocketException e) {
-			System.err.println("Failed to get an external interface address");
-		}
-		if(addr == null) {
-			System.err.println("No valid external interface have been found, " +
-				"falling back to loopback");
-			addr = InetAddress.getLoopbackAddress();
-		}
-		return addr.getHostAddress();
-	}
 
-	public static void create(final Service svc) {
-		try {
-			UnicastRemoteObject.exportObject(svc, 0);
-			System.out.println("Exported service object successfully");
-			final String svcName = svc.getName();
-			final String svcUri = svcUri(svcName).toString();
-			if(!SVCS.containsKey(svcUri)) {
-				Naming.rebind(svcUri, svc);
-				SVCS.put(svcName, svc);
-				System.out.println("New service bound " + svcUri);
-			} else {
-				throw new IllegalStateException("Duplication of service name");
-			}
-		} catch(final RemoteException e) {
-			try {
-				System.err.println("Failed to export service object " + svc.getName());
-			} catch(RemoteException ignore) {
-			}
-		} catch(final MalformedURLException e) {
-			System.err.println("Invalid service URL");
+	public static void create(final Service svc)
+	throws RemoteException, MalformedURLException, SocketException,
+		OmgLookAtMyConsoleException, OmgDoesNotPerformException {
+		UnicastRemoteObject.exportObject(svc, 0);
+		final String svcName = svc.getName();
+		final String svcUri = svcUri(svcName).toString();
+		if(!SVCS.containsKey(svcUri)) {
+			Naming.rebind(svcUri, svc);
+			SVCS.put(svcName, svc);
+		} else {
+			throw new IllegalStateException("Duplication of service name");
 		}
 	}
 
 	@SuppressWarnings("unchecked")
-	public static <S extends Service> S getSvc(final String name) {
+	public static <S extends Service> S getSvc(final String name)
+	throws RemoteException, NotBoundException, MalformedURLException, SocketException,
+		OmgLookAtMyConsoleException, OmgDoesNotPerformException {
 		final String svcUri = svcUri(name).toString();
-		try {
-			return (S) Naming.lookup(svcUri);
-		} catch(final NotBoundException e) {
-			System.err.println("No service bound with URL " + svcUri);
-		} catch(final MalformedURLException e) {
-			System.err.println("Invalid service URL " + svcUri);
-		} catch(final RemoteException e) {
-			System.err.println("Failed to get service");
-		}
-		return null;
+		return (S) Naming.lookup(svcUri);
 	}
 
 	public static void close(final Service svc)
-	throws RemoteException {
-		try {
-			UnicastRemoteObject.unexportObject(svc, true);
-			System.out.println("Unexported service object");
-		} catch(final NoSuchObjectException e) {
-			System.err.println("Failed to unexport service object");
-		}
-
+	throws RemoteException, MalformedURLException, NotBoundException, SocketException,
+		OmgLookAtMyConsoleException, OmgDoesNotPerformException {
+		UnicastRemoteObject.unexportObject(svc, true);
 		final String svcUri = svcUri(svc.getName()).toString();
-		try {
-			Naming.unbind(svcUri);
-			SVCS.remove(svcUri);
-			System.out.println("Removed service: " + svcUri);
-		} catch(final NotBoundException e) {
-			System.err.println("Service not bound");
-		} catch(final MalformedURLException e) {
-			System.err.println("Invalid service URL " + svcUri);
-		}
+		Naming.unbind(svcUri);
+		SVCS.remove(svcUri);
 	}
 
-	public static void shutdown() {
+	public static void shutdown()
+	throws RemoteException, NotBoundException, MalformedURLException, SocketException,
+		OmgLookAtMyConsoleException, OmgDoesNotPerformException {
 		for(final Service svc : SVCS.values()) {
-			try {
-				close(svc);
-			} catch(final RemoteException e) {
-				System.err.println("Failed to shutdown service");
-			}
+			close(svc);
 		}
 	}
 
