4e05894e5ae0a43a24638d0b73614522857dc7d8
Andrey Kurilov <andrey.kurilov@emc.com>
2016-12-02 01:09:02 +0300

    save

---
+++ storage/driver/service/src/main/java/com/emc/mongoose/storage/driver/service/WrappingInputSvc.java
@@ -0,0 +1,9 @@
+package com.emc.mongoose.storage.driver.service;
+
+import com.emc.mongoose.model.item.Item;
+
+/**
+ Created by andrey on 02.12.16.
+ */
+public final class WrappingInputSvc<I extends Item> {
+}
