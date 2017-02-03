c05174f4a19603fdc8f52bfcc3aab546be399411
Dmitry Kossovich <dmitry.kossovich@emc.com>
2017-02-03 20:11:40 +0300

    midterm commit -added Channel creation -experement client side implementation

---
+++ storage/mock/src/main/java/com/emc/mongoose/storage/mock/impl/base/ProtoStorageMockServer.java
@@ -0,0 +1,7 @@
+package com.emc.mongoose.storage.mock.impl.base;
+
+/**
+ * Created by reddy on 02.02.17.
+ */
+public class ProtoStorageMockServer {
+}
---
+++ storage/mock/src/main/java/com/emc/mongoose/storage/mock/impl/http/ChannelFactory.java
@@ -0,0 +1,7 @@
+package com.emc.mongoose.storage.mock.impl.http.request;
+
+/**
+ * Created by reddy on 03.02.17.
+ */
+public class ChannelFabric {
+}
