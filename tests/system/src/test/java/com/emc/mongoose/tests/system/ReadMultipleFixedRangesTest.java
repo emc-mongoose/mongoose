a4f255a30aa5f88cf61d3854e8a9de966f896a7a
Andrey Kurilov <andrey.kurilov@emc.com>
2017-02-28 18:42:35 +0300

    more ignored system tests

--- tests/system/src/test/java/com/emc/mongoose/tests/system/ReadMultipleFixedRangesTest.java
+++ tests/system/src/test/java/com/emc/mongoose/tests/system/ReadMultipleFixedRangesTest.java
@@ -118,7 +118,7 @@ extends HttpStorageDistributedScenarioTestBase {
 		);
 	}
 
-	@Test
+	@Test @Ignore
 	public void testTotalMetricsLogFile()
 	throws Exception {
 		final List<CSVRecord> totalMetrcisLogRecords = getMetricsTotalLogRecords();
