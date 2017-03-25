6604ae361e9818c7eb282d0ec2f26c1859b2896e
Andrey Kurilov <andrey.kurilov@emc.com>
2017-03-26 00:25:36 +0300

    1st stage of storage mock separation

--- tests/system/src/test/java/com/emc/mongoose/tests/system/MixedLoadTest.java
+++ tests/system/src/test/java/com/emc/mongoose/tests/system/MixedLoadTest.java
@@ -1,7 +1,8 @@
 package com.emc.mongoose.tests.system;
 
-import com.emc.mongoose.load.monitor.metrics.MetricsStdoutLogMessage;
+import com.emc.mongoose.model.io.IoType;
 import com.emc.mongoose.tests.system.base.HttpStorageDistributedScenarioTestBase;
+import com.emc.mongoose.tests.system.util.PortListener;
 import com.emc.mongoose.ui.log.LogUtil;
 import com.emc.mongoose.ui.log.appenders.LoadJobLogFileManager;
 import org.apache.logging.log4j.Level;
@@ -17,22 +18,39 @@ import java.util.concurrent.TimeUnit;
 
 import static com.emc.mongoose.common.Constants.KEY_STEP_NAME;
 import static com.emc.mongoose.common.env.PathUtil.getBaseDir;
-import static com.emc.mongoose.load.monitor.metrics.MetricsStdoutLogMessage.TABLE_BORDER;
-import static com.emc.mongoose.load.monitor.metrics.MetricsStdoutLogMessage.TABLE_HEADER;
 import static com.emc.mongoose.run.scenario.Scenario.DIR_SCENARIO;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 /**
  Created by kurila on 23.03.17.
+ Covered Use Cases:
+ * 2.1.1.1.3. Intermediate Size Data Items (100KB-10MB)
+ * 2.3.2. Items Output File
+ * 2.3.3.1. Constant Items Destination Path
+ * 4.3. Medium Concurrency Level (11-100)
+ * 5. Circularity
+ * 6.2.5. Limit Load Job by Time
+ * 7.1. Metrics Periodic Reporting
+ * 8.2.1. Create New Items
+ * 8.3.1. Read With Disabled Validation
+ * 9.1. Scenarios Syntax
+ * 9.4.1. Override Default Configuration in the Scenario
+ * 9.4.3. Reusing The Items in the Scenario
+ * 9.5.3. Precondition Load Job
+ * 9.5.7.1. Separate Configuration in the Mixed Load Job
+ * 10.1.2. Many Local Separate Storage Driver Services (at different ports)
  */
 public class MixedLoadTest
 extends HttpStorageDistributedScenarioTestBase {
 	private static final Path SCENARIO_PATH = Paths.get(
 		getBaseDir(), DIR_SCENARIO, "mixed", "mixed.json"
 	);
+	private static final int EXPECTED_CONCURRENCY = 20 + 50;
 	
 	private static boolean FINISHED_IN_TIME;
 	private static String STD_OUTPUT;
+	private static int ACTUAL_CONCURRENCY;
 	
 	@BeforeClass
 	public static void setUpClass()
@@ -57,7 +75,13 @@ extends HttpStorageDistributedScenarioTestBase {
 			}
 		);
 		runner.start();
-		TimeUnit.SECONDS.timedJoin(runner, 100);
+		TimeUnit.SECONDS.sleep(30); // warmup
+		final int startPort = CONFIG.getStorageConfig().getNetConfig().getNodeConfig().getPort();
+		for(int i = 0; i < STORAGE_NODE_COUNT; i ++) {
+			ACTUAL_CONCURRENCY += PortListener
+				.getCountConnectionsOnPort("127.0.0.1:" + (startPort + i));
+		}
+		TimeUnit.SECONDS.timedJoin(runner, 50);
 		FINISHED_IN_TIME = !runner.isAlive();
 		runner.interrupt();
 		LoadJobLogFileManager.flush(JOB_NAME);
@@ -74,10 +98,17 @@ extends HttpStorageDistributedScenarioTestBase {
 	public void testFinishedInTime() {
 		assertTrue("Scenario didn't finished in time", FINISHED_IN_TIME);
 	}
+
+	@Test
+	public void testActualConcurrency() {
+		assertEquals(STORAGE_DRIVERS_COUNT * EXPECTED_CONCURRENCY, ACTUAL_CONCURRENCY, 5);
+	}
 	
 	@Test
 	public void testMetricsStdout()
 	throws Exception {
-		testMetricsTableStdout(STD_OUTPUT);
+		testMetricsTableStdout(
+			STD_OUTPUT, STORAGE_DRIVERS_COUNT, 0, IoType.CREATE, IoType.READ
+		);
 	}
 }
--- tests/system/src/test/java/com/emc/mongoose/tests/system/base/LoggingTestBase.java
+++ tests/system/src/test/java/com/emc/mongoose/tests/system/base/LoggingTestBase.java
@@ -9,6 +9,7 @@ import com.emc.mongoose.tests.system.util.LogPatterns;
 import com.emc.mongoose.ui.log.LogUtil;
 import static com.emc.mongoose.common.Constants.K;
 import static com.emc.mongoose.common.Constants.KEY_STEP_NAME;
+import static com.emc.mongoose.common.Constants.MIB;
 import static com.emc.mongoose.common.env.DateUtil.FMT_DATE_ISO8601;
 
 import org.apache.commons.csv.CSVFormat;
@@ -43,6 +44,7 @@ import java.net.URL;
 import java.nio.file.Paths;
 import java.text.ParseException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
@@ -513,8 +515,10 @@ public abstract class LoggingTestBase {
 		}
 	}
 	
-	protected static void testMetricsTableStdout(final String stdOutContent)
-	throws Exception {
+	protected static void testMetricsTableStdout(
+		final String stdOutContent, final int driverCount, final long countLimit,
+		final IoType... ioTypes
+	) throws Exception {
 		
 		final List<String[]> records = new ArrayList<>();
 		
@@ -534,11 +538,53 @@ public abstract class LoggingTestBase {
 		}
 		
 		String cells[];
+		boolean ioTypeFoundFlag;
+		long countSucc;
+		int lat, dur;
+		float nextJobTime = -1, prevJobTime = -1;
+		float tp, bw;
+		SizeInBytes size;
 		for(final String rec[] : records) {
 			for(final String row : rec) {
 				cells = CELL_BORDER.split(WHITESPACES.matcher(row).replaceAll(""));
-				System.out.println(cells.length);
+				final IoType actualIoType = IoType.valueOf(cells[0]);
+				ioTypeFoundFlag = false;
+				for(final IoType nextIoType : ioTypes) {
+					if(nextIoType.equals(actualIoType)) {
+						ioTypeFoundFlag = true;
+						break;
+					}
+				}
+				assertTrue(
+					"I/O type \"" + actualIoType + "\" not found, expected one of: " +
+					Arrays.toString(ioTypes), ioTypeFoundFlag
+				);
+				assertTrue(0 < Integer.parseInt(cells[1])); // concurrency
+				assertEquals(driverCount, Integer.parseInt(cells[2])); // driver count
+				countSucc = Long.parseLong(cells[3]);
+				if(countLimit > 0) {
+					assertTrue(countLimit > countSucc); // count succ
+				}
+				assertTrue(0 == Long.parseLong(cells[4])); // count fail
+				nextJobTime = Float.parseFloat(cells[5]); // job time
+				if(prevJobTime >= 0) {
+					assertEquals(10, nextJobTime - prevJobTime, 1);
+				}
+				tp = Float.parseFloat(cells[6]); // TP mean
+				if(nextJobTime > 0) {
+					assertEquals(countSucc / nextJobTime, tp, tp / 100);
+				}
+				size = new SizeInBytes(cells[8]);
+				bw = Float.parseFloat(cells[9]); // BW mean
+				if(tp > 0) {
+					assertEquals(MIB * bw / tp, size.getAvg() / countSucc, size.getAvg() / 1000);
+				}
+				lat = Integer.parseInt(cells[11]); // latency
+				assertTrue(lat >= 0);
+				dur = Integer.parseInt(cells[12]); // duration
+				assertTrue(dur >= lat);
 			}
+			prevJobTime = nextJobTime;
 		}
 	}
 }
