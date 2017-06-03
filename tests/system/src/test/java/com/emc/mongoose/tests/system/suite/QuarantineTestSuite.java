94432fb79747cd653a3cc1e7457f7dfc8a879efc
Andrey Kurilov <andrey.kurilov@emc.com>
2017-06-04 00:11:17 +0300

    quarantine test suite update

--- tests/system/src/test/java/com/emc/mongoose/tests/system/suite/QuarantineTestSuite.java
+++ tests/system/src/test/java/com/emc/mongoose/tests/system/suite/QuarantineTestSuite.java
@@ -1,19 +1,23 @@
 package com.emc.mongoose.tests.system.suite;
 
 import com.emc.mongoose.tests.system.CreateByCountTest;
+import com.emc.mongoose.tests.system.ReadBucketListingTest;
 import com.emc.mongoose.tests.system.ReadMultipleRandomFileRangesTest;
 import com.emc.mongoose.tests.system.ReadSmallDataItemsMetricsThresholdTest;
 import com.emc.mongoose.tests.system.S3MpuTest;
 
+import org.junit.Ignore;
 import org.junit.runners.Suite;
 import org.junit.runner.RunWith;
 
 /**
  Created by andrey on 03.06.17.
  */
+@Ignore
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
 	CreateByCountTest.class,
+	ReadBucketListingTest.class,
 	ReadMultipleRandomFileRangesTest.class,
 	ReadSmallDataItemsMetricsThresholdTest.class,
 	S3MpuTest.class,
