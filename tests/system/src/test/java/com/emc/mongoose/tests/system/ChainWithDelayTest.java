99e8de5fcdfe3e01cfc472026e569eb9728f47d9
Andrey Kurilov <andrey.kurilov@emc.com>
2017-07-04 20:35:48 +0300

    fix merge typo

--- tests/system/src/test/java/com/emc/mongoose/tests/system/ChainWithDelayTest.java
+++ tests/system/src/test/java/com/emc/mongoose/tests/system/ChainWithDelayTest.java
@@ -7,6 +7,7 @@ import com.emc.mongoose.run.scenario.JsonScenario;
 import com.emc.mongoose.tests.system.base.EnvConfiguredScenarioTestBase;
 import com.emc.mongoose.tests.system.util.EnvUtil;
 import com.emc.mongoose.ui.log.LogUtil;
+
 import static com.emc.mongoose.common.Constants.KEY_TEST_STEP_ID;
 import static com.emc.mongoose.common.Constants.M;
 import static com.emc.mongoose.common.env.PathUtil.getBaseDir;
@@ -66,7 +67,7 @@ extends EnvConfiguredScenarioTestBase {
 	throws Exception {
 		EnvUtil.set("ZONE1_ADDRS", ZONE1_ADDR);
 		EnvUtil.set("ZONE2_ADDRS", ZONE2_ADDR);
-		ThreadContext.put(KEY_STEP_NAME, STEP_NAME);
+		ThreadContext.put(KEY_TEST_STEP_ID, STEP_NAME);
 		CONFIG_ARGS.add("--storage-net-http-namespace=ns1");
 		CONFIG_ARGS.add("--test-step-limit-time=" + TIME_LIMIT);
 		EnvConfiguredScenarioTestBase.setUpClass();
