d2babc9a13f5f6595242cf41c76b9b818abdbcbc
Andrey Kurilov <andrey.kurilov@emc.com>
2017-07-04 20:07:33 +0300

    performance experiment

--- storage/driver/net/src/main/java/com/emc/mongoose/storage/driver/net/base/NetStorageDriverBase.java
+++ storage/driver/net/src/main/java/com/emc/mongoose/storage/driver/net/base/NetStorageDriverBase.java
@@ -107,7 +107,7 @@ implements NetStorageDriver<I, O>, ChannelPoolHandler {
 		final int workerCount;
 		final int confWorkerCount = storageConfig.getDriverConfig().getIoConfig().getWorkers();
 		if(confWorkerCount < 1) {
-			workerCount = Math.min(concurrencyLevel, 2 * ThreadUtil.getHardwareThreadCount());
+			workerCount = Math.min(concurrencyLevel, ThreadUtil.getHardwareThreadCount());
 		} else {
 			workerCount = confWorkerCount;
 		}
