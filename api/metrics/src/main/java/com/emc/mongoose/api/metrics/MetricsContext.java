5137bdc9978ceaca37e167135ca7a321ae429bc1
Andrey Kurilov <andrey.kurilov@emc.com>
2017-09-02 01:00:01 +0300

    + performance experiment [skip ci]

--- api/metrics/src/main/java/com/emc/mongoose/api/metrics/MetricsContext.java
+++ api/metrics/src/main/java/com/emc/mongoose/api/metrics/MetricsContext.java
@@ -12,8 +12,7 @@ import java.io.Serializable;
 public interface MetricsContext
 extends Closeable {
 
-	int DEFAULT_RESERVOIR_SIZE = 1000;
-	int DEFAULT_DISTRIBUTION_SNAPSHOT_UPDATE_PERIOD_MILLIS = 10;
+	int DEFAULT_DISTRIBUTION_SNAPSHOT_UPDATE_PERIOD_MILLIS = 100;
 
 	void start();
 	boolean isStarted();
