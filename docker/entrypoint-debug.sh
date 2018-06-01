0599b1b0a95e7765e231359a265f23c333b7ffe1
Andrey Kurilov <andrey.kurilov@emc.com>
2018-06-01 23:10:38 +0300

    + dockerfile fix [skip ci]

--- docker/entrypoint-debug.sh
+++ docker/entrypoint-debug.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 umask 0000
-java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005 -jar /opt/mongoose/mongoose.jar "$@"
+java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005 -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=9010 -Dcom.sun.management.jmxremote.rmi.port=9010 -Dcom.sun.management.jmxremote.local.only=false -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -jar /opt/mongoose/mongoose.jar "$@"
