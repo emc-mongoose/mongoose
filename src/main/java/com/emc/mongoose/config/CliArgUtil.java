be3547de04f02496131a645da0b3f4adc6fcb4aa
kochuv <veronika.kochugova@gmail.ru>
2018-07-04 17:32:51 +0300

    in progress

--- src/main/java/com/emc/mongoose/config/CliArgUtil.java
+++ src/main/java/com/emc/mongoose/config/CliArgUtil.java
@@ -8,63 +8,64 @@ import java.util.stream.Collectors;
 
 public interface CliArgUtil {
 
-	String ARG_PREFIX = "--";
-	String ARG_PATH_SEP = "-";
-	String ARG_VAL_SEP = "=";
+    String ARG_PREFIX = "--";
+    String ARG_PATH_SEP = "-";
+    String ARG_VAL_SEP = "=";
 
-	static Map<String, String> parseArgs(final String... args) {
-		return Arrays
-			.stream(args)
-			.map(
-				arg -> {
-					if(arg.startsWith(ARG_PREFIX)) {
-						return arg
-							.substring(ARG_PREFIX.length())
-							.split(ARG_VAL_SEP, 2);
-					} else {
-						throw new IllegalArgumentNameException(arg);
-					}
-				}
-			)
-			.map(
-				argValuePair -> argValuePair.length == 2 ?
-					argValuePair : new String[] { argValuePair[0], Boolean.TRUE.toString() }
-			)
-			.collect(
-				Collectors.toMap(
-					argValuePair -> argValuePair[0],
-					argValuePair -> argValuePair[1]
-				)
-			);
-	}
+    static Map<String, String> parseArgs(final String... args) {
+        return Arrays
+                .stream(args)
+                .map(
+                        arg -> {
+                            if (arg.startsWith(ARG_PREFIX)) {
+                                return arg
+                                        .substring(ARG_PREFIX.length())
+                                        .split(ARG_VAL_SEP, 2);
+                            } else {
+                                throw new IllegalArgumentNameException(arg);
+                            }
+                        }
+                )
+                .map(
+                        argValuePair -> argValuePair.length == 2 ?
+                                argValuePair : new String[]{argValuePair[0], Boolean.TRUE.toString()}
+                )
+                .peek(argValuePair -> System.out.println(argValuePair[0]))
+                .collect(
+                        Collectors.toMap(
+                                argValuePair -> argValuePair[0],
+                                argValuePair -> argValuePair[1]
+                        )
+                );
+    }
 
-	@SuppressWarnings("CollectionWithoutInitialCapacity")
-	static List<String> allCliArgs(final Map<String, Object> schema, final String sep) {
-		final List<String> allArgs = new ArrayList<>();
-		schema
-			.entrySet()
-			.stream()
-			.map(schemaEntry -> _argsFromSchemaEntry(ARG_PREFIX, sep, schemaEntry))
-			.forEach(allArgs::addAll);
-		return allArgs;
-	}
+    @SuppressWarnings("CollectionWithoutInitialCapacity")
+    static List<String> allCliArgs(final Map<String, Object> schema, final String sep) {
+        final List<String> allArgs = new ArrayList<>();
+        schema
+                .entrySet()
+                .stream()
+                .map(schemaEntry -> _argsFromSchemaEntry(ARG_PREFIX, sep, schemaEntry))
+                .forEach(allArgs::addAll);
+        return allArgs;
+    }
 
-	@SuppressWarnings({ "CollectionWithoutInitialCapacity", "unchecked" })
-	static List<String> _argsFromSchemaEntry(
-		final String prefix, final String sep, final Map.Entry<String, Object> schemaEntry
-	) {
-		final List<String> args = new ArrayList<>();
-		final String schemaKey = schemaEntry.getKey();
-		final Object schemaVal = schemaEntry.getValue();
-		if(schemaVal instanceof Map) {
-			((Map<String, Object>) schemaVal)
-				.entrySet()
-				.stream()
-				.map(e -> _argsFromSchemaEntry(prefix + schemaKey + sep, sep, e))
-				.forEach(args::addAll);
-		} else {
-			args.add(prefix + schemaKey + ARG_VAL_SEP + '<' + schemaVal + '>');
-		}
-		return args;
-	}
+    @SuppressWarnings({"CollectionWithoutInitialCapacity", "unchecked"})
+    static List<String> _argsFromSchemaEntry(
+            final String prefix, final String sep, final Map.Entry<String, Object> schemaEntry
+    ) {
+        final List<String> args = new ArrayList<>();
+        final String schemaKey = schemaEntry.getKey();
+        final Object schemaVal = schemaEntry.getValue();
+        if (schemaVal instanceof Map) {
+            ((Map<String, Object>) schemaVal)
+                    .entrySet()
+                    .stream()
+                    .map(e -> _argsFromSchemaEntry(prefix + schemaKey + sep, sep, e))
+                    .forEach(args::addAll);
+        } else {
+            args.add(prefix + schemaKey + ARG_VAL_SEP + '<' + schemaVal + '>');
+        }
+        return args;
+    }
 }
