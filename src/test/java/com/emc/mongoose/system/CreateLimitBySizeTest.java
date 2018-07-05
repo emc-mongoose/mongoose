e685d3ee644aee5fcfda8f7001e02580f9321173
kochuv <veronika.kochugova@gmail.ru>
2018-07-05 11:47:42 +0300

    in progress

--- src/test/java/com/emc/mongoose/system/CreateLimitBySizeTest.java
+++ src/test/java/com/emc/mongoose/system/CreateLimitBySizeTest.java
@@ -330,11 +330,11 @@ public class CreateLimitBySizeTest {
 //                    put(IoType.CREATE, concurrency.getValue());
 //                }}
 //        );
-//        testFinalMetricsTableRowStdout(
-//                stdOutContent, stepId, IoType.CREATE, runMode.getNodeCount(), concurrency.getValue(),
-//                0, 0, itemSize.getValue()
-//        );
-//
+        testFinalMetricsTableRowStdout(
+                stdOutContent, stepId, IoType.CREATE, runMode.getNodeCount(), concurrency.getValue(),
+                0, 0, itemSize.getValue()
+        );
+
         assertTrue(duration < timeoutInMillis);
     }
 }
