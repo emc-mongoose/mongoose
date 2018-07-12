7aa22a38eeb98f9bb9e31804c21574c8b2b6907d
kochuv <veronika.kochugova@gmail.ru>
2018-07-12 11:52:49 +0300

    create ReadCustomContentVerificationFailTest

--- src/main/java/com/emc/mongoose/env/MainInstaller.java
+++ src/main/java/com/emc/mongoose/env/MainInstaller.java
@@ -64,6 +64,7 @@ public final class MainInstaller
                     "example/scenario/js/systest/multipart_create_test.js",
                     "example/scenario/js/systest/multiple_fixed_update_and_single_fixed_read_test.js",
                     "example/scenario/js/systest/multiple_random_update_and_single_fixed_read_test.js",
+                    "example/scenario/js/systest/read_custom_content_verification_fail_test.js",
 
                     // custom scenario examples in python
                     "example/scenario/py/types/additional/copy_load_using_env_vars.py",
