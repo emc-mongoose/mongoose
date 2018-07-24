78bf71a5099917b4cc321cd7ffd038b5d385cf91
Veronika <veronika.kochugova@gmail.com>
2018-07-24 10:52:16 +0300

    added PATH-overriding into entypoint.sh [skip ci]

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 umask 0000
+export PATH=/usr/local/jdk/bin:/usr/kerberos/sbin:/usr/kerberos/bin:/usr/local/sbin:usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/opt/cpanel/composer/bin:/usr/local/easy/bin:/usr/local/bin:/usr/X11R6/bin:/root/bin
 java -jar /opt/mongoose/mongoose.jar "$@"
