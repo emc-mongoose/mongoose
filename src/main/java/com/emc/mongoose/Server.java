9c6d0a4e672925ef90c02cb75fd3aa08496c3f55
veronikaKochugova <veronika.kochugova@gmail.com>
2018-10-08 15:26:29 +0300

    save [skip ci]

--- src/main/java/com/emc/mongoose/Server.java
+++ src/main/java/com/emc/mongoose/Server.java
@@ -4,6 +4,8 @@ import io.prometheus.client.exporter.MetricsServlet;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 
+import java.util.Arrays;
+
 /**
  @author veronika K. on 05.10.18 */
 public class Server {
@@ -34,6 +36,13 @@ public class Server {
 	public void stop() {
 		try {
 			server.stop();
+			Arrays.asList(server.getChildHandlers()).stream().peek(handler -> {
+				try {
+					handler.stop();
+				} catch(Exception e) {
+					e.printStackTrace();
+				}
+			});
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
