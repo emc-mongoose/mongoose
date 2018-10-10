24dab3ed43ca22e28624fd78a35e515ed5016f28
veronikaKochugova <veronika.kochugova@gmail.com>
2018-10-10 13:13:00 +0300

    save [skip ci]

--- src/main/java/com/emc/mongoose/Server.java
+++ src/main/java/com/emc/mongoose/Server.java
@@ -7,13 +7,17 @@ import org.eclipse.jetty.servlet.ServletHolder;
 
 /**
  @author veronika K. on 05.10.18 */
-public class Server extends AsyncRunnableBase {
+public class Server
+	extends AsyncRunnableBase {
 
 	private final org.eclipse.jetty.server.Server server;
 
 	public Server(final int port) {
+		this(port, new ServletContextHandler());
+	}
+
+	public Server(final int port, final ServletContextHandler context) {
 		server = new org.eclipse.jetty.server.Server(port);
-		ServletContextHandler context = new ServletContextHandler();
 		context.setContextPath("/");
 		server.setHandler(context);
 		context.addServlet(new ServletHolder(new MetricsServlet()), "/metrics");
