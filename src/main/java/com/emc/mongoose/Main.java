4b3da9fa5eee516ad4daa5db004f1e23005f26a4
veronikaKochugova <veronika.kochugova@gmail.com>
2018-10-11 15:21:29 +0300

    delete Server class [skip ci]

--- src/main/java/com/emc/mongoose/Main.java
+++ src/main/java/com/emc/mongoose/Main.java
@@ -20,8 +20,12 @@ import com.github.akurilov.confuse.Config;
 import com.github.akurilov.confuse.SchemaProvider;
 import com.github.akurilov.confuse.exceptions.InvalidValuePathException;
 import com.github.akurilov.confuse.exceptions.InvalidValueTypeException;
+import io.prometheus.client.exporter.MetricsServlet;
 import org.apache.commons.lang.StringUtils;
 import org.apache.logging.log4j.Level;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
 
 import javax.script.ScriptEngine;
 import javax.script.ScriptException;
@@ -80,7 +84,13 @@ public final class Main {
 				//TODO: add port to defaults
 				//final int port = configWithArgs.intVal("run-port");
 				final int port = 1234;
-				final Server server = new Server(port).start();
+				final Server server = new Server(port);
+				final ServletContextHandler context = new ServletContextHandler();
+				context.setContextPath("/");
+				server.setHandler(context);
+				context.addServlet(new ServletHolder(new MetricsServlet()), "/metrics");
+				server.start();
+				server.join();
 				//
 				try {
 					run(configWithArgs, extensions, extClsLoader, appHomePath);
