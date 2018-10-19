918f5118e6e9c135bf504c338bae9767ea362ed8
veronikaKochugova <veronika.kochugova@gmail.com>
2018-10-19 15:56:17 +0300

    RollingAppenderTest [skip ci]

--- src/test/java/com/emc/mongoose/integration/RollingAppenderTest.java
+++ src/test/java/com/emc/mongoose/integration/RollingAppenderTest.java
@@ -1,10 +1,10 @@
 package com.emc.mongoose.integration;
 
 import org.apache.log4j.Appender;
-import org.apache.log4j.FileAppender;
 import org.apache.log4j.Logger;
 import org.apache.log4j.RollingFileAppender;
 import org.apache.log4j.SimpleLayout;
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.io.File;
@@ -14,31 +14,32 @@ import java.util.concurrent.TimeUnit;
 public class RollingAppenderTest {
 
 	private static final int LIMIT = 5;
-	private static String fileName = "/home/user/mongoose/log4jFileTest.txt";
+	private static String fileName = "/tmp/log4jFileTest.txt";
 	public static Logger logger = Logger.getLogger(RollingAppenderTest.class);
 	public static Appender appender;
 
 	@Test
 	public void test()
 	throws InterruptedException, IOException {
-//		appender = new RollingFileAppender(new SimpleLayout(), fileName);
-//		logger.addAppender(appender);
-//		for(int counter = 0; counter < LIMIT; ++ counter) {
-//			TimeUnit.SECONDS.sleep(1);
-//			System.out.println(counter);
-//			RollingAppenderTest.logger.info(counter);
-//		}
-//		new File(fileName).delete();
-//		System.out.println("<File is deleted>");
-//		//logger.removeAllAppenders();
-//		//appender = new RollingFileAppender(new SimpleLayout(), fileName);
-//		//logger.addAppender(appender);
-//
-//		for(int counter = LIMIT; counter < LIMIT * 2; ++ counter) {
-//			TimeUnit.SECONDS.sleep(1);
-//			System.out.println(counter);
-//			RollingAppenderTest.logger.info(counter);
-//		}
+		appender = new RollingFileAppender(new SimpleLayout(), fileName);
+		logger.addAppender(appender);
+		for(int counter = 0; counter < LIMIT; ++ counter) {
+			TimeUnit.SECONDS.sleep(1);
+			System.out.println(counter);
+			RollingAppenderTest.logger.info(counter);
+		}
+		Assert.assertEquals("The file is not created", true, new File(fileName).exists());
+		new File(fileName).delete();
+		//
+		System.out.println("<File is deleted>");
+		//
+		for(int counter = LIMIT; counter < LIMIT * 2; ++ counter) {
+			TimeUnit.SECONDS.sleep(1);
+			System.out.println(counter);
+			RollingAppenderTest.logger.info(counter);
+		}
+		//
+		Assert.assertEquals("The file is not created after deletion: Log4j2 bug", false, new File(fileName).exists());
 	}
 }
 
