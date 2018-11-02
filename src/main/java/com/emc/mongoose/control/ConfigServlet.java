252c6d8aad4a449d48989b64264f79c6866e7537
veronikaKochugova <veronika.kochugova@gmail.com>
2018-11-02 13:22:53 +0300

    save [skip ci]

--- src/main/java/com/emc/mongoose/control/ConfigServlet.java
+++ src/main/java/com/emc/mongoose/control/ConfigServlet.java
@@ -1,14 +1,20 @@
 package com.emc.mongoose.control;
 
 import com.emc.mongoose.config.ConfigUtil;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.github.akurilov.confuse.Config;
+import com.github.akurilov.confuse.io.json.TypeNames;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Map;
 
 /**
  @author veronika K. on 26.10.18 */
@@ -42,9 +48,24 @@ public class ConfigServlet
 	private void getSchema(final HttpServletResponse resp)
 	throws IOException {
 		resp.setStatus(STATUS_OK);
-		resp.getWriter().print(new ObjectMapper()
+		resp.getWriter().print(schemaSerialize(config.schema()));
+	}
+
+	private String schemaSerialize(final Map schema)
+	throws JsonProcessingException {
+		final SimpleModule module = new SimpleModule();
+		for(String key : TypeNames.MAP.keySet()) {
+			module.addSerializer(TypeNames.MAP.get(key), new TypeJsonSerializer<>(TypeNames.MAP.get(key).getClass()));
+		}
+		final ObjectMapper m = new ObjectMapper()
+			.registerModule(module);
+//			.enable(DeserializationFeature.FAIL_ON_NULL_FOR_PRIMITIVES)
+//			.enable(DeserializationFeature.FAIL_ON_READING_DUP_TREE_KEY)
+//			.configure(JsonParser.Feature.ALLOW_COMMENTS, true)
+//			.configure(JsonParser.Feature.ALLOW_YAML_COMMENTS, true);
+		return m
 			.writerWithDefaultPrettyPrinter()
-			.writeValueAsString(config.schema()));
+			.writeValueAsString(schema);
 	}
 
 	private void getConfig(final HttpServletResponse resp)
