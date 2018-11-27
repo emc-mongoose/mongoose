52de22065870ef80386bd76c32a8587e3b99a1c9
veronikaKochugova <veronika.kochugova@gmail.com>
2018-11-27 20:05:53 +0300

    hook check [skip ci]

--- src/main/java/com/emc/mongoose/svc/Service.java
+++ src/main/java/com/emc/mongoose/svc/Service.java
@@ -1,21 +1,13 @@
 package com.emc.mongoose.svc;
 
 import com.github.akurilov.commons.concurrent.AsyncRunnable;
-
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
-/**
- Created by kurila on 07.05.14.
- A remote service which has a name for resolution by URI.
- */
+/** Created by kurila on 07.05.14. A remote service which has a name for resolution by URI. */
+public interface Service extends AsyncRunnable, Remote {
 
-public interface Service
-extends AsyncRunnable, Remote {
+  int registryPort() throws RemoteException;
 
-	int registryPort(test)
-	throws RemoteException;
-
-	String name()
-	throws RemoteException;
+  String name() throws RemoteException;
 }
