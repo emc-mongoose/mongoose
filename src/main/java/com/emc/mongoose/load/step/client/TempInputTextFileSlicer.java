7d4a00099e92f843282dc7887918856b85f46eba
Andrey Kurilov <andrey.kurilov@emc.com>
2018-11-30 16:54:12 +0300

    try #28

--- src/main/java/com/emc/mongoose/load/step/client/TempInputTextFileSlicer.java
+++ src/main/java/com/emc/mongoose/load/step/client/TempInputTextFileSlicer.java
@@ -199,7 +199,7 @@ implements AutoCloseable {
 						lineQueues.get((int) (lineCount % sliceCount)).put(line);
 						lineCount ++;
 					}
-					System.out.println(lineCount);
+					Loggers.MSG.trace(lineCount);
 				}
 
 			} catch(final IOException e) {
