12358902189c4d3ce6375f445e5dc119023945f7
Andrey Kurilov <andrey.kurilov@emc.com>
2018-12-02 10:00:30 +0300

    try #44

--- docker/entrypoint_legacy_systest.sh
+++ docker/entrypoint_legacy_systest.sh
@@ -1,10 +1,3 @@
 #!/bin/sh
 umask 0000
-docker --version
-echo Mongoose Version: ${MONGOOSE_VERSION}
-echo Storage Type: ${STORAGE_TYPE}
-echo Run Mode: ${RUN_MODE}
-echo Concurrency Limit: ${CONCURRENCY}
-echo Item Size: ${ITEM_SIZE}
-echo Test: ${TEST}
 ./gradlew clean systemTest --tests com.emc.mongoose.system.${TEST}
