09ae60c9531fa48ab90b5345c2efb4038e4737c0
Andrey Kurilov <andrey.kurilov@emc.com>
2018-12-02 22:37:30 +0300

    try #46

--- docker/entrypoint_robotest.sh
+++ docker/entrypoint_robotest.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
 umask 0000
 robot --outputdir /root/mongoose/build/robotest --suite ${SUITE} --include ${TEST} /root/mongoose/src/test/robot
-ls -l /root
-ls -l /root/mongoose
-ls -l /root/mongoose/build
-ls -l /root/mongoose/build/robotest
 rebot /root/mongoose/build/robotest/output.xml
