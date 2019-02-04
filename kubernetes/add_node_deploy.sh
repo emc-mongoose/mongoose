1d02d19d0d42a7c4d2d9bcf848065ad93c9bbdae
veronikaKochugova <veronika.kochugova@gmail.com>
2019-02-04 11:20:57 +0300

    try [skip ci]

--- kubernetes/add_node_deploy.sh
+++ kubernetes/add_node_deploy.sh
@@ -5,7 +5,7 @@ systemctl enable kubelet && systemctl start kubelet && systemctl status kubelet
 
 echo Environment="KUBELET_EXTRA_ARGS=--fail-swap-on=false" >> /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
 systemctl daemon-reload && systemctl restart kubelet
-yum update
+y | yum update
 
 yes | kubeadm reset
 
--- kubernetes/master_node_create.sh
+++ kubernetes/master_node_create.sh
@@ -18,5 +18,7 @@ yes | cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 #chown $(id -u):$(id -g) $HOME/.kube/config
 kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
 
+kubectl taint nodes --all node-role.kubernetes.io/master-
+
 #check
 kubectl cluster-info
